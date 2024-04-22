ac51bf91a542d7e9932a342de7b70c9eb6aa4e3e
Tom Fontaine <tom.fnt9@gmail.com>
2024-04-22 18:50:53 +0200

    Add homeController for the homepage & patch wrong use declaration for Entity Project in ProjectRepository.php

--- src/Repository/ProjectRepository.php
+++ src/Repository/ProjectRepository.php
@@ -2,27 +2,27 @@
 
 namespace App\Repository;
 
-use App\Entity\Projet;
+use App\Entity\Project;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<Projet>
+ * @extends ServiceEntityRepository<Project>
  *
- * @method Projet|null find($id, $lockMode = null, $lockVersion = null)
- * @method Projet|null findOneBy(array $criteria, array $orderBy = null)
- * @method Projet[]    findAll()
- * @method Projet[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method Project|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Project|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Project[]    findAll()
+ * @method Project[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class ProjectRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Projet::class);
+        parent::__construct($registry, Project::class);
     }
 
     //    /**
-    //     * @return Projet[] Returns an array of Projet objects
+    //     * @return Project[] Returns an array of Project objects
     //     */
     //    public function findByExampleField($value): array
     //    {
@@ -36,7 +36,7 @@ class ProjectRepository extends ServiceEntityRepository
     //        ;
     //    }
 
-    //    public function findOneBySomeField($value): ?Projet
+    //    public function findOneBySomeField($value): ?Project
     //    {
     //        return $this->createQueryBuilder('p')
     //            ->andWhere('p.exampleField = :val')
