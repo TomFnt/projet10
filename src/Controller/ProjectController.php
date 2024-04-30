12606b3724712e25467bfc92d8a57f01e4ed7c02
Tom Fontaine <tom.fnt9@gmail.com>
2024-04-30 19:07:55 +0200

    WIP Begin creation of project routing

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -2,23 +2,33 @@
 
 namespace App\Controller;
 
-use App\Repository\ProjectRepository;
+use App\Entity\Project;
+use App\Repository\TaskRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ProjectController
 {
-    #[Route( name: 'project_show')]
-    public function index(ProjectRepository $projectRepository): Response
-    {
-        return true;
-    }
-    #[Route( name: 'project_add')]
+    #[Route("/project/add", name: 'project_add')]
     public function add(): Response
     {
         return true;
     }
-    #[Route( name: 'project_edit')]
+
+    #[Route("/project/{id}", name: 'project_index', requirements: ['id' => '\d+'])]
+    public function index(Project $project, TaskRepository $taskRepository): Response
+    {
+
+        $idProject = $project->getIdProject();
+        $tasks = $taskRepository->findTasksByProject();
+
+        return $this->render('project/project_index.html.twig', [
+            "project" => $project,
+            "tasks" => $tasks
+        ]);
+    }
+
+    #[Route("/project/edit/{id}", name: 'project_edit')]
     public function edit(): Response
     {
         return true;
--- src/Repository/TaskRepository.php
+++ src/Repository/TaskRepository.php
@@ -21,20 +21,21 @@ class TaskRepository extends ServiceEntityRepository
         parent::__construct($registry, Task::class);
     }
 
-    //    /**
-    //     * @return Task[] Returns an array of Task objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('t')
-    //            ->andWhere('t.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('t.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+
+    /**
+     * Get all Taks for a specific Project
+     * @param $value, correspond to id_project
+     * @return array
+     */
+    public function findTasksByProject(int $value): array
+        {
+            return $this->createQueryBuilder('t')
+                ->andWhere('t.project = :val')
+                ->setParameter('val', $value)
+                ->orderBy('t.id_task', 'ASC')
+                ->getQuery()
+                ->getResult();
+        }
 
     //    public function findOneBySomeField($value): ?Task
     //    {
