5ab8a8adb1edd9e85ab5b36b8d08ba128bbe6502
TomFnt <tom@fnt9@gmail.com>
2024-05-03 16:43:17 +0200

    Create route "project_add"

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -3,27 +3,56 @@
 namespace App\Controller;
 
 use App\Entity\Project;
+use App\Form\ProjectType;
 use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ProjectController extends AbstractController
 {
-    #[Route('/project/add', name: 'project_add')]
-    public function projectAdd(): Response
+    public function __construct(private EntityManagerInterface $entityManager)
     {
-        return true;
+    }
+
+    #[Route('/project/add', name: 'project_add')]
+    public function projectAdd(Request $request): Response
+    {
+        $project = new Project();
+
+        $form = $this->createForm(ProjectType::class, $project);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $project = $form->getData();
+            try {
+                $this->entityManager->persist($project);
+                $this->entityManager->flush();
+                $this->addFlash('success', 'Votre nouveau projet a été enregistrées avec succès.');
+            } catch (\Exception $message) {
+                $this->addFlash('error', "Une erreur c'est produite lors de la création de ce projet. ".$message);
+
+                return $this->redirectToRoute('project_add');
+            }
+
+            return $this->redirectToRoute('project_index', ['id' => $project->getId()]);
+        }
+
+        return $this->render('project/project-form.html.twig', [
+            'form' => $form,
+            'page_title' => 'Créer un projet',
+            'btn_label' => 'Créer',
+        ]);
     }
 
     #[Route('/project/{id}', name: 'project_index', requirements: ['id' => '\d+'])]
     public function projectIndex(Project $project, TaskRepository $taskRepository): Response
     {
-
-        $toDoList = $taskRepository->findBy(['project' => $project, 'status' =>'To Do'], ['deadline'=>'ASC']);
-        $DoingList = $taskRepository->findBy(['project' => $project, 'status' =>'Doing'], ['deadline'=>'ASC']);
-        $doneList = $taskRepository->findBy(['project' => $project, 'status' =>'Done'], ['deadline'=>'ASC']);
-
+        $toDoList = $taskRepository->findBy(['project' => $project, 'status' => 'To Do'], ['deadline' => 'ASC']);
+        $DoingList = $taskRepository->findBy(['project' => $project, 'status' => 'Doing'], ['deadline' => 'ASC']);
+        $doneList = $taskRepository->findBy(['project' => $project, 'status' => 'Done'], ['deadline' => 'ASC']);
 
         return $this->render('project/project_index.html.twig', [
             'project' => $project,
