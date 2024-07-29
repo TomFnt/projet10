862d5e3b4ab4f065491ad33db77b049194aae15a
TomFnt <tom.fnt9@gmail.com>
2024-07-29 18:59:08 +0200

    Implement UniqueEntity for email value in Employee.php, in order to avoid the risk duplicated email for the auth system

--- src/Entity/Employee.php
+++ src/Entity/Employee.php
@@ -7,11 +7,13 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: EmployeeRepository::class)]
+#[UniqueEntity('email')]
 class Employee implements UserInterface, PasswordAuthenticatedUserInterface
 {
     public const statutEmployeeList = ['CDI', 'CDD', 'Freelance', 'Alternant'];
