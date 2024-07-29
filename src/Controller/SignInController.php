f647974bf20d54ae81026c3f06b05ae092affe34
TomFnt <tom.fnt9@gmail.com>
2024-07-29 18:47:55 +0200

    Implement SignIn FormType and use it in base-login.html.twig and SignInController.php . Hash password in SignInController.php before registration in BDD

--- src/Controller/SignInController.php
+++ src/Controller/SignInController.php
@@ -2,18 +2,54 @@
 
 namespace App\Controller;
 
+use App\Form\SignInType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SignInController extends AbstractController
 {
-    #[Route('/sign/in', name: 'app_sign_in')]
-    public function index(): Response
+    public function __construct(private EntityManagerInterface $entityManager)
     {
+    }
+
+    #[Route('/sign/in', name: 'app_sign_in')]
+    public function index(Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $form = $this->createForm(SignInType::class);
+
+        $form->handleRequest($request);
+
+        // Check if  $form are submitted & if value are valid
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $employee = $form->getData();
+                $employee->setAvatar($employee->getName(), $employee->getSurname());
+                $employee->setStatus(1);
+
+                $hashPassword = $passwordHasher->hashPassword($employee, $employee->getPassword());
+                $employee->setPassword($hashPassword);
+
+                $date = new \DateTime();
+                $employee->setDateAdd($date);
+                var_dump($employee);
+                $this->entityManager->persist($employee);
+                $this->entityManager->flush();
+                $this->addFlash('success', "L'inscription a été enregistrée avec succès.");
+            } catch (\Exception $message) {
+                $this->addFlash('error', "Une erreur c'est produite lors de l'inscription.".$message);
+            }
+
+            return $this->redirectToRoute('app_home');
+        }
+
         return $this->render('sign_in/base-login.html.twig', [
-            'controller_name' => 'SignInController',
+            'form' => $form->createView(),
             'page_title' => 'Sign in',
+            'btn_label'=>'Sign in'
         ]);
     }
 }
