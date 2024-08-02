7e1ea7ea5530782bf67617c0cbd0e64190f249c7
TomFnt <tom.fnt9@gmail.com>
2024-08-02 22:09:33 +0200

    Refactor SignInController.php, SignInType.php and view base-login.html.twig in order to correspond to "Sign Up" & keep an unique Controller for login logic

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -2,13 +2,20 @@
 
 namespace App\Controller;
 
+use App\Form\SignUpType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AuthController extends AbstractController
 {
+    public function __construct(private EntityManagerInterface $entityManager)
+    {
+    }
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -21,13 +28,49 @@ class AuthController extends AbstractController
         return $this->render('auth/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
-            'page_title' => 'Authentification'
+            'page_title' => 'Sign in',
         ]);
     }
-
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route('/signup', name: 'app_sign_up')]
+    public function signup(Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $form = $this->createForm(SignUpType::class);
+
+        $form->handleRequest($request);
+
+        // Check if  $form are submitted & if value are valid
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $employee = $form->getData();
+                $employee->setAvatar($employee->getName(), $employee->getSurname());
+                $employee->setStatus(1);
+
+                $hashPassword = $passwordHasher->hashPassword($employee, $employee->getPassword());
+                $employee->setPassword($hashPassword);
+
+                $date = new \DateTime();
+                $employee->setDateAdd($date);
+                var_dump($employee);
+                $this->entityManager->persist($employee);
+                $this->entityManager->flush();
+                $this->addFlash('success', "L'inscription a été enregistrée avec succès.");
+            } catch (\Exception $message) {
+                $this->addFlash('error', "Une erreur c'est produite lors de l'inscription.".$message);
+            }
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('auth/base-login.html.twig', [
+            'form' => $form->createView(),
+            'page_title' => 'Sign up',
+            'btn_label'=>'Sign up'
+        ]);
     }
 }
--- src/Form/SignUpType.php
+++ src/Form/SignUpType.php
@@ -10,7 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class SignInType extends AbstractType
+class SignUpType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
